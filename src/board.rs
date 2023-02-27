b72e338a342cfc2620eac2ff05b54f9ec0f19f06
Daniel Larson <vortexofdoom97@gmail.com>
2023-02-27 14:48:54 -0600

    cleaned up

--- src/board.rs
+++ src/board.rs
@@ -1,10 +1,7 @@
-use grid::Grid;
 use anyhow::{anyhow, Result};
+use grid::Grid;
 
-const EMPTY: [char; 9]  = [
-    '1', '2', '3', 
-    '4', '5', '6', 
-    '7', '8', '9'];
+const EMPTY: [char; 9] = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 #[derive(Debug, Clone)]
 pub struct Board {
@@ -15,7 +12,13 @@ impl std::fmt::Display for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let grid = &self.grid;
         let print_row = |row| format!("{}|{}|{}\n", grid[row][0], grid[row][1], grid[row][2]);
-        write!(f, "{}-+-+-\n{}-+-+-\n{}\n", print_row(0), print_row(1), print_row(2))
+        write!(
+            f,
+            "{}-+-+-\n{}-+-+-\n{}\n",
+            print_row(0),
+            print_row(1),
+            print_row(2)
+        )
     }
 }
 
@@ -30,13 +33,15 @@ impl Board {
     pub const P2: char = 'O';
 
     pub fn player(first: bool) -> char {
-        if first { Board::P1 } else { Board::P2 }
+        if first {
+            Board::P1
+        } else {
+            Board::P2
+        }
     }
 
     pub fn new() -> Self {
-        Self {         
-            grid: Grid::from_vec(Vec::from(EMPTY), 3)
-        }
+        Self::from_vec(Vec::from(EMPTY))
     }
 
     pub fn from_vec(vec: Vec<char>) -> Self {
@@ -52,26 +57,81 @@ impl Board {
             .map(|(i, _)| i + 1)
             .collect()
     }
+    
+    pub fn is_full(&self) -> bool {
+        self.get_open_spaces().len() == 0
+    }
 
+    // Jank
     pub fn iter(&self) -> impl Iterator<Item = &char> {
         self.grid.iter()
     }
 
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut char> {
+        self.grid.iter_mut()
+    }
+
     pub fn reset(&mut self) {
-        for (i, c) in self.grid.iter_mut().enumerate() {
+        for (i, c) in self.iter_mut().enumerate() {
             *c = EMPTY[i];
         }
     }
 
+    pub fn check_matches(&self) -> Option<char> {
+        // no one likes typing self
+        let board = &self.grid;
+        // first check diagonals
+        let diag = |a: char, b: char| a == b && a == board[1][1];
+        if diag(board[0][0], board[2][2]) || diag(board[0][2], board[2][0]) {
+            return Some(board[1][1]);
+        }
+
+        // Then check rows and columns
+        for i in 0..3 {
+            let cell = board[i][i]; // (i, i) is the coordinate that will be common to both row and column i for comparison
+            if board.iter_row(i).all(|c| c == &cell) || board.iter_col(i).all(|c| c == &cell) {
+                return Some(cell);
+            }
+        }
+
+        // No match found
+        None
+    }
+
+    pub fn get_cell(&self, i: usize) -> Result<Option<char>> {
+        if i == 0 || i > 9 {
+            return Err(anyhow!("Number selected must be between 1 and 9"));
+        }
+        let row = (i - 1) / 3;
+        let col = (i - 1) % 3;
+        if let Some(&c) = self.grid.get(row, col) {
+            if c.is_alphabetic() {
+                return Ok(Some(c));
+            }
+        }
+        Ok(None)
+    }
+
+    pub fn set_cell(&mut self, i: usize, first: bool) -> Result<()> {
+        let player = if first { Self::P1 } else { Self::P2 };
+        if self.get_cell(i)?.is_none() {
+            let i = i - 1;
+            Ok(self.grid[i / 3][i % 3] = player)
+        } else {
+            Err(anyhow!("cell already occupied"))
+        }
+    }
+
+    // Returns a genericized representation of the board, with open spaces represented by '0'
     fn state(&self) -> Board {
-        Self::from_vec(self
-            .iter()
-            .map(|c| match c {
-                'X' => '1',
-                'O' => '2',
-                _ => '0',
-            })
-            .collect()
+        Self::from_vec(
+            self.iter()
+                .map(|&c| match c {
+                    Self::P1 => '1',
+                    Self::P2 => '2',
+                    _ => '0',
+                })
+                .collect(),
         )
     }
 
@@ -79,30 +139,21 @@ impl Board {
         let row0 = self.grid.iter_row(0);
         let row1 = self.grid.iter_row(1);
         let row2 = self.grid.iter_row(2);
-        Self::from_vec(row2
-            .chain(row1)
-            .chain(row0)
-            .cloned()
-            .collect()
-        )
-        
+        Self::from_vec(row2.chain(row1).chain(row0).cloned().collect())
     }
 
     pub fn flip_h(&self) -> Board {
         let row0 = self.grid.iter_row(0).rev();
         let row1 = self.grid.iter_row(1).rev();
         let row2 = self.grid.iter_row(2).rev();
-        Self::from_vec(row0
-            .chain(row1)
-            .chain(row2)
-            .cloned()
-            .collect()
-        )
+        Self::from_vec(row0.chain(row1).chain(row2).cloned().collect())
     }
 
     pub fn transpose(&self) -> Board {
-        Board { grid: self.grid.transpose() }
-    } 
+        Board {
+            grid: self.grid.transpose(),
+        }
+    }
 
     pub fn state_key(&self) -> String {
         let base = self.state();
@@ -118,66 +169,14 @@ impl Board {
         states.push(base.flip_h().flip_v().iter().collect());
         // Rotate 90 degrees R
         states.push(base.transpose().flip_h().iter().collect());
+        // Transpose rows and columns
         states.push(base.transpose().iter().collect());
+        // Transpose and rotate 180 degrees
         states.push(base.transpose().flip_v().flip_h().iter().collect());
+        // Take whichever permutation results in the smallest string
         states.into_iter().min().unwrap()
     }
 
-    pub fn check_matches(&self) -> Option<char> {
-        // no one likes typing self
-        let board = &self.grid;
-        // first check diagonals
-        let diag = |a: char, b: char| a == b && a == board[1][1];
-        if diag(board[0][0], board[2][2]) { 
-            return Some(board[0][0]);
-        }
-        if diag(board[0][2], board[2][0]) {
-            return Some(board[0][2]);
-        }
-
-        // Then check rows and columns
-        for i in 0..3 {
-            // (i, i) is the coordinate that will be common to both row and column i for comparison
-            let cell = board[i][i];
-            if board.iter_row(i).all(|c| c == &cell) || board.iter_col(i).all(|c| c == &cell) {
-                return Some(cell);
-            }
-        }
-
-        // No match found
-        None
-    }
-    
-    pub fn is_full(&self) -> bool {
-        self.get_open_spaces().len() == 0
-    }
-
-    pub fn get_cell(&self, i: usize) -> Result<Option<char>> {
-        if i == 0 || i > 9 {
-            return Err(anyhow!("Number selected must be between 1 and 9"));
-        }
-        let row = (i - 1) / 3;
-        let col = (i - 1) % 3;
-        if let Some(&c) = self.grid.get(row, col) {
-            if c.is_alphabetic() {
-                Ok(Some(c))
-            } else {
-                Ok(None)
-            }
-        } else {
-            Ok(None)
-        }
-    }
-
-    pub fn set_cell(&mut self, i: usize, first: bool) -> Result<()> {
-        let player = if first { Self::P1 } else { Self::P2 };
-        if self.get_cell(i)?.is_none() {
-            let i = i - 1;
-            Ok(self.grid[i / 3][i % 3] = player)
-        } else {
-            Err(anyhow!("cell already occupied"))
-        }
-    }
 }
 
 #[cfg(test)]
@@ -187,57 +186,33 @@ mod tests {
     fn should_have_used_nalgebra() {
         let board = Board::new();
         assert_eq!(
-            board.flip_h(), 
-            Board::from_vec(vec![
-                '3', '2', '1',
-                '6', '5', '4',
-                '9', '8', '7'
-            ])
-        );
-        
-        assert_eq!(
-            board.flip_v(), 
-            Board::from_vec(vec![
-                '7', '8', '9',
-                '4', '5', '6', 
-                '1', '2', '3'
-            ])
+            board.flip_h(),
+            Board::from_vec(vec!['3', '2', '1', '6', '5', '4', '9', '8', '7'])
         );
 
         assert_eq!(
-            board.transpose().flip_v(), 
-            Board::from_vec(vec![
-                '3', '6', '9',
-                '2', '5', '8', 
-                '1', '4', '7'
-            ])
+            board.flip_v(),
+            Board::from_vec(vec!['7', '8', '9', '4', '5', '6', '1', '2', '3'])
         );
 
         assert_eq!(
-            board.flip_h().flip_v(), 
-            Board::from_vec(vec![
-                '9', '8', '7',
-                '6', '5', '4', 
-                '3', '2', '1'
-            ])
+            board.transpose().flip_v(),
+            Board::from_vec(vec!['3', '6', '9', '2', '5', '8', '1', '4', '7'])
         );
 
         assert_eq!(
-            board.transpose().flip_h(), 
-            Board::from_vec(vec![
-                '7', '4', '1',
-                '8', '5', '2', 
-                '9', '6', '3'
-            ])
+            board.flip_h().flip_v(),
+            Board::from_vec(vec!['9', '8', '7', '6', '5', '4', '3', '2', '1'])
+        );
+
+        assert_eq!(
+            board.transpose().flip_h(),
+            Board::from_vec(vec!['7', '4', '1', '8', '5', '2', '9', '6', '3'])
         );
 
         assert_eq!(
             board.transpose().flip_v().flip_h(),
-            Board::from_vec(vec![
-                '9', '6', '3',
-                '8', '5', '2', 
-                '7', '4', '1'
-            ])
+            Board::from_vec(vec!['9', '6', '3', '8', '5', '2', '7', '4', '1'])
         );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,16 @@ use rand::Rng;
 
 mod board;
 
-const THREE_IN_A_ROW: [[usize; 3]; 8] = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+const THREE_IN_A_ROW: [[usize; 3]; 8] = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+];
 
 #[derive(Debug, Clone, Copy)]
 enum Player {
@@ -28,27 +37,8 @@ struct Game {
 }
 
 impl Game {
-    pub fn get_y_n_input(msg: &str, buf: &mut String) -> bool {
-        loop {
-            buf.clear();
-            println!("{msg} Y/N: ");
-            std::io::stdin()
-                .read_line(buf)
-                .expect("failed to read line");
-    
-            match buf.trim().to_lowercase().as_str() {
-                "y" | "yes" => return true,
-                "n" | "no" => return false,
-                _ => {
-                    println!("Invalid input! ");
-                    continue;
-                }
-            }
-        }
-    }
-
     pub fn new() -> Self {
-        Game { 
+        Game {
             board: Board::new(),
             p1: Player::Human,
             p2: Player::Human,
@@ -58,44 +48,9 @@ impl Game {
         }
     }
 
-    pub fn set_player(&mut self, buf: &mut String) -> Result<()> {
-        let first = self.p1_turn;
-        let player = if first {
-            &mut self.p1
-        } else {
-            &mut self.p2
-        };
-        let query = format!("Player {} human?", Board::player(first));
-
-        if !Self::get_y_n_input(&query, buf) {
-            loop {
-                buf.clear();
-                println!("Select AI difficulty (1-3): ");
-                std::io::stdin()
-                    .read_line(buf)
-                    .expect("could not read line");
-                match buf.trim().parse::<i32>() {
-                    Ok(1) => *player = Player::Random,
-                    Ok(2) => *player = Player::FindWinning,
-                    Ok(3) => *player = Player::BlockLosing,
-                    Ok(4) => *player = Player::Optimal,
-                    _ => continue,
-                }
-                break;
-            }
-        }
-        self.p1_turn = !self.p1_turn;
-        Ok(())
-    }
-
-    pub fn get_move(&mut self, player: Player, buf: &mut String) -> usize {
-        match player {
-            Player::Human => self.get_input(buf),
-            Player::Random => self.pick_random_open_move(),
-            Player::FindWinning => self.only_find_winning(),
-            Player::BlockLosing => self.find_winning_block_losing(),
-            Player::Optimal => self.pick_optimal_move(),
-        }
+    pub fn render(&self) {
+        print!("\x1b[H\x1b[J");
+        println!("{}", self.board);
     }
 
     pub fn reset(&mut self) {
@@ -112,9 +67,60 @@ impl Game {
         }
     }
 
-    pub fn render(&self) {
-        print!("\x1b[H\x1b[J");
-        println!("{}", self.board);
+    pub fn get_y_n_input(msg: &str, buf: &mut String) -> bool {
+        loop {
+            buf.clear();
+            println!("{msg} Y/N: ");
+            std::io::stdin()
+                .read_line(buf)
+                .expect("failed to read line");
+
+            match buf.trim().to_lowercase().as_str() {
+                "y" | "yes" => return true,
+                "n" | "no" => return false,
+                _ => {
+                    println!("Invalid input! ");
+                    continue;
+                }
+            }
+        }
+    }
+
+    pub fn set_player(&mut self, buf: &mut String) -> Result<()> {
+        let first = self.p1_turn;
+        let player = if first { &mut self.p1 } else { &mut self.p2 };
+        let query = format!("Player {} human?", Board::player(first));
+
+        if !Self::get_y_n_input(&query, buf) {
+            loop {
+                buf.clear();
+                println!("Select AI difficulty (1-4): ");
+                std::io::stdin()
+                    .read_line(buf)
+                    .expect("could not read line");
+                match buf.trim().parse::<i32>() {
+                    Ok(1) => *player = Player::Random,
+                    Ok(2) => *player = Player::FindWinning,
+                    Ok(3) => *player = Player::BlockLosing,
+                    Ok(4) => *player = Player::Optimal,
+                    _ => continue,
+                }
+                break;
+            }
+        }
+        // flip it so we don't set the same player twice
+        self.p1_turn = !self.p1_turn;
+        Ok(())
+    }
+
+    pub fn get_move(&mut self, player: Player, buf: &mut String) -> usize {
+        match player {
+            Player::Human => self.get_input(buf),
+            Player::Random => self.pick_random_open_move(),
+            Player::FindWinning => self.only_find_winning(),
+            Player::BlockLosing => self.find_winning_block_losing(),
+            Player::Optimal => self.pick_optimal_move(),
+        }
     }
 
     pub fn run(&mut self, buf: &mut String) {
@@ -136,15 +142,57 @@ impl Game {
     }
 
     // move input functions
+
+    // checks the board to see if there are any winning or losing moves.
+    // returns a Vec containing all such moves along with whether or not they are a winning move
+    fn check_win_loss(&self) -> Vec<(usize, bool)> {
+        let cell = |i| self.board.get_cell(i).expect("invalid index");
+        let check_win_loss = |a, b, c| match [cell(a), cell(b), cell(c)] {
+            [Some(x), Some(y), None] => {
+                if x == y {
+                    Some((c, x == Board::player(self.p1_turn)))
+                } else {
+                    None
+                }
+            }
+            [Some(x), None, Some(y)] => {
+                if x == y {
+                    Some((b, x == Board::player(self.p1_turn)))
+                } else {
+                    None
+                }
+            }
+            [None, Some(x), Some(y)] => {
+                if x == y {
+                    Some((a, x == Board::player(self.p1_turn)))
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        };
+
+        THREE_IN_A_ROW
+            .iter()
+            .map(|[a, b, c]| check_win_loss(*a, *b, *c))
+            .filter(|i| i.is_some())
+            .map(|i| i.unwrap())
+            .collect()
+    }
+
+    // Get human input
     pub fn get_input(&mut self, buf: &mut String) -> usize {
         loop {
             buf.clear();
-            println!("Player {}, please select an empty cell 1-9: ", Board::player(self.p1_turn));
+            println!(
+                "Player {}, please select an empty cell 1-9: ",
+                Board::player(self.p1_turn)
+            );
             std::io::stdin()
                 .read_line(buf)
                 .expect("could not read line");
             if let Ok(n @ 1..=9) = buf.trim().parse::<usize>() {
-                if self.board.get_open_spaces().contains(&n){
+                if self.board.get_open_spaces().contains(&n) {
                     return n;
                 }
             }
@@ -157,34 +205,7 @@ impl Game {
         *open.get(guess).unwrap()
     }
 
-    fn check_win_loss(&self) -> Vec<(usize, bool)> {
-        let cell = |i| self.board.get_cell(i).expect("invalid index");
-        let check_win_loss = |a,b,c| match [cell(a), cell(b), cell(c)] {
-            [Some(x), Some(y), None] => if x == y {
-                Some((c, x == Board::player(self.p1_turn)))
-            } else {
-                None
-            },
-            [Some(x), None, Some(y)] => if x == y {
-                Some((b, x == Board::player(self.p1_turn)))
-            } else {
-                None
-            },
-            [None, Some(x), Some(y)] => if x == y {
-                Some((a, x == Board::player(self.p1_turn)))
-            } else {
-                None
-            },
-            _ => None
-        };
-
-        THREE_IN_A_ROW.iter()
-            .map(|[a, b, c]| check_win_loss(*a, *b, *c))
-            .filter(|i| i.is_some())
-            .map(|i| i.unwrap())
-            .collect()
-    }
-    
+    // finds winning moves and ignores losing ones
     fn only_find_winning(&self) -> usize {
         for (i, b) in self.check_win_loss() {
             if b {
@@ -194,8 +215,8 @@ impl Game {
         self.pick_random_open_move()
     }
 
+    // Prioritize winning over blocking a loss over picking at random
     fn find_winning_block_losing(&self) -> usize {
-        // Prioritize winning over blocking a loss over picking at random
         match self.check_win_loss().iter().find_or_first(|(_, b)| *b) {
             Some((best, _)) => *best,
             None => self.pick_random_open_move(),
@@ -205,21 +226,15 @@ impl Game {
     fn pick_optimal_move(&mut self) -> usize {
         let first = self.p1_turn;
         let open = self.board.get_open_spaces();
-        let scores = open
-            .iter()
-            .map(|i| {
-                let mut board = self.board.clone();
-                board.set_cell(*i, first).unwrap();
-                (i, self.minimax_score(!first, &board))
-            });
+        let scores = open.iter().map(|i| {
+            let mut board = self.board.clone();
+            board.set_cell(*i, first).unwrap();
+            (i, self.minimax_score(!first, &board))
+        });
         if first {
-            scores.max_by_key(|(_, m)| *m)
-            .map(|(i, _)| *i)
-            .unwrap()
+            scores.max_by_key(|(_, m)| *m).map(|(i, _)| *i).unwrap()
         } else {
-            scores.min_by_key(|(_, m)| *m)
-            .map(|(i, _)| *i)
-            .unwrap()
+            scores.min_by_key(|(_, m)| *m).map(|(i, _)| *i).unwrap()
         }
     }
 
@@ -228,32 +243,32 @@ impl Game {
         if let Some(&s) = self.cache.get(&state) {
             return s;
         }
-        if let Some(w) = board.check_matches() {
+        if let Some(w @ (Board::P1 | Board::P2)) = board.check_matches() {
             match w {
-                'X' => return 10,
-                _ => return -10,
+                Board::P1 => return 10,
+                Board::P2 => return -10,
+                _ => return i32::MAX, // categorically impossible
             }
         } else if board.is_full() {
             return 0;
         };
         let options = board.get_open_spaces();
-        let res = options
-            .iter()
-            .map(|i| {
-                let mut board = board.clone();
-                board.set_cell(*i, first).unwrap();
-                self.minimax_score(!first, &board)
-            });
+        let res = options.iter().map(|i| {
+            // create a board taking each move
+            let mut board = board.clone();
+            board.set_cell(*i, first).unwrap();
+            // return the optimal opponent score for each move
+            self.minimax_score(!first, &board)
+        });
         let score = match first {
             true => res.max().unwrap(),
             false => res.min().unwrap(),
         };
+        // update the cache with the result
         self.cache.insert(state, score);
         score
     }
 }
-
-
 
 fn main() {
     let mut buf = String::new();
